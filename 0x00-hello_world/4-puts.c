75f09830aa70116388730744587ece6e694372c1
Olumight10 <olumight69@gmail.com>
2022-06-30 17:05:13 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+* Description: using the main function
+* Return: 0
+*/
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+
+}
