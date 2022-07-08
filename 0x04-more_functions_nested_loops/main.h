efccdea44ea3a50b3b1a2f8e33d709ec2d0f57fc
Olumight10 <olumight69@gmail.com>
2022-07-07 17:48:43 -0700

    more funcions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,4 +7,32 @@ int _isdigit(int c);
 
 int mul(int a, int b);
 
-#endif
+void print_numbers(void);
+
+
+void print_most_numbers(void);
+
+
+
+void more_numbers(void);
+
+
+
+void print_line(int n);
+
+
+
+void print_diagonal(int n);
+
+
+
+void print_square(int size);
+
+
+
+void print_triangle(int size);
+
+
+
+void print_number(int n);/
+endif
